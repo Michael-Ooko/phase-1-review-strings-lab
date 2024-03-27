5c98ed1e482b52414e1e260dda2360c51c533a9c
Michael <oliengo.michael@gmail.com>
2024-03-27 05:33:48 +0300

    submits strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code in this file!
+// // Test 1
+// const currentUser = 'Grace Hopper';
+// // Test 2
+// const welcomeMessage = 'Welcome to Flatbook, ';
+// Test 3
+// const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+// Test 4
+// const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// Test 5
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// test 6
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
